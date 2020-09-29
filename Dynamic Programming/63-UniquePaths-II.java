ee8925ecdfabcb2e8cfd5dfc09baee4ec160a02f
Rishabh Jaiswal <rshbh.jaiswal@gmail.com>
2020-09-30 00:50:21 +0530

    Unique Paths-I , II, III LeetCode Problems

---
+++ Dynamic Programming/63-UniquePaths-II.java
@@ -0,0 +1,58 @@
+/*
+https://leetcode.com/problems/unique-paths-ii/
+
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output: 2
+Explanation:
+There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+*/
+
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        
+    int dp[][]= new int[m][n];
+    for(int i=0;i<m;i++)
+    {
+        if(obstacleGrid[i][0]==1)
+            break;
+        dp[i][0]=1;
+    }
+        
+    for(int j=0;j<n;j++)
+    {
+        if(obstacleGrid[0][j]==1)
+            break;
+        dp[0][j]=1;
+    }
+        
+    for(int i=1;i<m;i++)
+    {
+        for(int j=1;j<n;j++)
+        {
+            if(obstacleGrid[i][j]==0)
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        }
+    }
+    
+    return dp[m-1][n-1];
+
+
+    }
+}
